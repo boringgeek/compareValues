a1b6a18dbeb2f26822b384d468ecdaf50612dc0f
Curtis Rissi <curtis.rissi@gmail.com>
2015-10-28 10:04:10 -0700

    spelling errors....

--- compareValues.php
+++ compareValues.php
@@ -6,7 +6,7 @@ This example accepts two lines of input from STDIN
 The first line consists of two integers separated by a space. The first integer is a total number of values that we wish to compare. The second number is the deviation we want to find
 The second line consists of a series of integers separated by a space.
 
-The desire is to count the total number of integers whos are wtihin the specified deviation
+The desire is to count the total number of integers that are within the specified deviation
 Additionally, the desire is to minimize the computational time complexity as best you can
 
 */
